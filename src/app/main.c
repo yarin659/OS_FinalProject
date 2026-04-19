2df9cc6756cd593411c3f722bb7029e16b78083a
SleepyMode <sleepy@sleepymode.me>
2026-04-19 09:42:32 +0300

    add common.h to cmake, unit tests placeholder

--- src/app/main.c
+++ src/app/main.c
@@ -1,8 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "core/common.h"
 
 int main(int argc, char *argv[]) {
     UNREFERENCED_PARAMETER(argc);
     UNREFERENCED_PARAMETER(argv);
+
+    if (argc > 0 && !strcmp(argv[0], "--test")) {
+        // TODO run unit tests
+        return 0;
+    }
 
     return 0;
 }
