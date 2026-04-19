cf724e4369f8f48b52c33b51848275450fdf380f
SleepyMode <sleepy@sleepymode.me>
2026-04-19 11:09:42 +0300

    Add graph loading to main

--- src/app/main.c
+++ src/app/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include "core/common.h"
+#include "core/graph.h"
 
 int main(int argc, char *argv[]) {
     UNREFERENCED_PARAMETER(argc);
@@ -11,6 +12,9 @@ int main(int argc, char *argv[]) {
         // TODO run unit tests
         return 0;
     }
+
+    Graph graph = {};
+    load_graph_from_file("data/sample_graph.dat", &graph);
 
     return 0;
 }
